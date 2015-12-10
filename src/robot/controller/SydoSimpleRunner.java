1f03730afc2c360737c54799fbcc344cb5b09e43
Sydothekido11 <sydnemelka@gmail.com>
2015-12-10 13:54:10 -0700

    Created the controller and the constructors in it.

--- src/robot/controller/SydoSimpleRunner.java
+++ src/robot/controller/SydoSimpleRunner.java
@@ -2,5 +2,9 @@ package robot.controller;
 
 public class SydoSimpleRunner
 {
-
+	public static void main (String [] args)
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
 }
